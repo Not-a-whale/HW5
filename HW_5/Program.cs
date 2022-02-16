dea5faef3ccb2f8a5fd19e958a5e87b22dd128ca
nikita <sabaoth@ukr.net>
2022-02-16 23:16:59 +0200

    Enum created

---
+++ HW_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW_5
+{
+    class Program
+    {
+        public enum LogLevels 
+        {
+            Error, 
+            Info,
+            Warning
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+         
+
+    }
+}
