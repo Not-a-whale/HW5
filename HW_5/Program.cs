eb4401841aefed62a4dd9edd244f395243f08967
nikita <sabaoth@ukr.net>
2022-02-17 11:21:34 +0200

    Logs are ready. Date error needs to be fixed

--- HW_5/Program.cs
+++ HW_5/Program.cs
@@ -5,6 +5,7 @@ namespace HW_5
 {
     class Program
     {
+        private static Logger logger;
         public enum LogLevels 
         {
             Error, 
@@ -15,8 +16,8 @@ namespace HW_5
         public struct Result
         {
             public LogLevels status;
-            string message;
-            DateTime timestamp;
+            public string message;
+            public DateTime timestamp;
 
             const string errorMessage = "You've got an error!";
             const string infoMessage = "You've been informed!";
@@ -29,11 +30,11 @@ namespace HW_5
                 if (status == LogLevels.Error)
                 {
                     this.message = errorMessage;
-                }
+                } else 
                 if (status == LogLevels.Info)
                 {
                     this.message = infoMessage;
-                }
+                } else 
                 if (status == LogLevels.Warning)
                 {
                     this.message = warningMessage;
@@ -49,38 +50,123 @@ namespace HW_5
             private Result[] errorArr = new Result[] { };
             private Result[] infoArr = new Result[] { };
             private Result[] warningArr = new Result[] { };
-            private void LogItems()
-            {
 
+            private static Logger instance;
+
+            protected Logger(Result result) 
+            {
+                this.AddItem(result);
+            }
+
+            public static Logger GetInstance(Result res)
+            {
+                if(instance == null)
+                {
+                    instance = new Logger(res);
+                }
+                instance.AddItem(res);
+                return instance;
+            }
+            public void LogItems(Result res)
+            {
+                if(res.status == LogLevels.Error)
+                {
+                    foreach(Result log in this.errorArr)
+                    {
+                        Console.WriteLine($"{log.timestamp}: {log.status.ToString()}: {log.message}");
+                    }
+                } else 
+                if (res.status == LogLevels.Warning)
+                {
+                    foreach (Result log in this.warningArr)
+                    {
+                        Console.WriteLine($"{log.timestamp}: {log.status.ToString()}: {log.message}");
+                    }
+                } else 
+                if (res.status == LogLevels.Info)
+                {
+                    foreach (Result log in this.infoArr)
+                    {
+                        Console.WriteLine($"{log.timestamp}: {log.status.ToString()}: {log.message}");
+                    }
+                }
             }
 
             private void AddItem(Result result)
             {
-                Result[] currentArr;
-
                 if(result.status == LogLevels.Error)
                 {
-                    currentArr = this.errorArr;
-                }
+                    Array.Resize(ref this.errorArr, this.errorArr.Length + 1);
+                    this.errorArr[this.errorArr.Length - 1] = result;
+                } else
                 if (result.status == LogLevels.Info)
                 {
-                    currentArr = this.infoArr;
-                }
+                    Array.Resize(ref this.infoArr, this.infoArr.Length + 1);
+                    this.infoArr[this.infoArr.Length - 1] = result;
+                } else
                 if (result.status == LogLevels.Warning)
                 {
-                    currentArr = this.warningArr;
-                } else
-                {
-                    currentArr = this.errorArr;
+                    Array.Resize(ref this.warningArr, this.warningArr.Length + 1);
+                    this.warningArr[this.warningArr.Length - 1] = result;
                 }
-
-                Array.Resize(ref currentArr, currentArr.Length + 1);
-                currentArr[currentArr.GetUpperBound(0)] = result;
             }
         }
+
+        static class Actions
+        {
+            public static Result InfoAction()
+            {
+                return new Result(null, LogLevels.Info);
+            }
+
+            public static Result ErrorAction()
+            {
+                return new Result(null, LogLevels.Error);
+            }
+
+            public static Result WarningAction()
+            {
+                return new Result(null, LogLevels.Warning);
+            }
+        }
+
+        static class Starter
+        {
+            public static void Run()
+            {
+                Random r = new Random();
+                int randomNumber = r.Next(1, 10);
+
+                for(int i = 0; i <= randomNumber; i++)
+                {
+                    Result LogAction;
+                    Random randomNumForMethod = new Random();
+                    int randomMethodNumber = randomNumForMethod.Next(1, 4);
+                    if(randomMethodNumber == 1)
+                    {
+                        LogAction = Actions.InfoAction();
+                        Program.logger = Logger.GetInstance(LogAction);
+                        logger.LogItems(LogAction);
+                    }
+                    if(randomMethodNumber == 2)
+                    {
+                        LogAction = Actions.WarningAction();
+                        Program.logger = Logger.GetInstance(LogAction);
+                        logger.LogItems(LogAction);
+                    }
+                    if (randomMethodNumber == 3)
+                    {
+                        LogAction = Actions.ErrorAction();
+                        Program.logger = Logger.GetInstance(LogAction);
+                        logger.LogItems(LogAction);
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.Starter.Run();
         }
          
 
